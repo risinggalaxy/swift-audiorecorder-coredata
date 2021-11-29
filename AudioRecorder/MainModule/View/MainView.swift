9d3d6bc9fbcb1ebac8eccc31e5072f435f45725c
YASSER FARAHI <yasserfarahi@YASSERs-MBP.home>
2021-11-29 14:10:59 +0100

    First Commit

---
+++ AudioRecorder/MainModule/View/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AudioRecorder
+//
+//  Created by YASSER FARAHI on 29/11/2021.
+//
+
+import UIKit
+
+class MainView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
